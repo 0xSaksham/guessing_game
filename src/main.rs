c12198d53151304497bf1c1b4e78b7c881684e02
Saksham Gupta <sakshamgupta.ai@gmail.com>
2023-11-15 15:40:16 +0530

    Added Secret number matching

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,29 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main(){
     println!("Guess the Number!");
 
     println!("Please Input Your Guess: ");
 
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    println!("The Secret Number is: {secret_number}");
+    
     let mut guess = String::new();
 
     io::stdin().read_line(&mut guess)
     .expect("Failed to read line");
 
-    println!("You Guessed: {}",guess);
+    let guess:u32 = guess.trim().parse().expect("Please type a Number!");
+
+    println!("You Guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too Small!"),
+        Ordering::Greater => println!("Too Big!"),
+        Ordering::Equal => println!("You Win!"),
+        
+    }
 
 }
